5fcc600503eba8e35f0cc00ff79746c92c3182d1
Sachio471 <128691893+Sachio471@users.noreply.github.com>
2023-03-24 10:51:55 +0700

    Add files via upload

---
+++ tugas 1  webmediaatc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      cout<<"Hello Dunia!!!"<<endl;
+      cout<<"Saya mahasiswa semester          : 2"<<endl;
+      cout<<"Saya mahasiswa dari Universitas  : Universitas Sriwijaya!!!"<<endl;
+      cout<<"Saya berusia                  	 : 18 tahun"<<endl;
+      cout<<"Saya tinggal di                  : Perumahan the Green Borang Sako Palembang"<<endl;
+      cout<<"Saya memilki hobby               : mencoba hal baru "<<endl;
+      cout<<"Saya sedang belajar pemrograman C++"<<endl;
+    return 0;
+}
